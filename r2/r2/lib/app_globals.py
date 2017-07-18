4797eaf3056034c329db4ea7d86ab0d8e3918513
Nathan Levesque <nlevesq@mit.edu>
2017-07-18 10:00:26 -0400

    Fixed configure_tracing argument

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -411,7 +411,7 @@ class Globals(object):
         ],
         ConfigValue.dict(ConfigValue.str, ConfigValue.int): [
             'ticket_groups',
-            'ticket_user_fields', 
+            'ticket_user_fields',
         ],
         ConfigValue.dict(ConfigValue.str, ConfigValue.float): [
             'pennies_per_server_second',
@@ -449,7 +449,7 @@ class Globals(object):
             section in the config file for your application.
 
         ``extra``
-            The configuration returned from ``load_config`` in 
+            The configuration returned from ``load_config`` in
             ``config/middleware.py`` which may be of use in the setup of
             your global variables.
 
@@ -479,7 +479,7 @@ class Globals(object):
         self.baseplate.configure_logging()
         self.baseplate.register(R2BaseplateObserver())
         self.baseplate.configure_tracing(
-            service_name="r2",
+            "r2",
             tracing_endpoint=self.config.get("tracing_endpoint"),
             sample_rate=self.config.get("tracing_sample_rate"),
         )
@@ -487,7 +487,7 @@ class Globals(object):
         self.paths = paths
 
         self.running_as_script = global_conf.get('running_as_script', False)
-        
+
         # turn on for language support
         self.lang = getattr(self, 'site_lang', 'en')
         self.languages, self.lang_name = get_active_langs(
@@ -496,11 +496,11 @@ class Globals(object):
         all_languages = self.lang_name.keys()
         all_languages.sort()
         self.all_languages = all_languages
-        
+
         # set default time zone if one is not set
         tz = global_conf.get('timezone', 'UTC')
         self.tz = pytz.timezone(tz)
-        
+
         dtz = global_conf.get('display_timezone', tz)
         self.display_tz = pytz.timezone(dtz)
 
@@ -556,7 +556,7 @@ class Globals(object):
             self.config,
             self.pkg_resources_working_set,
             "r2.provider.support",
-            # TODO: fix this later, it refuses to pick up 
+            # TODO: fix this later, it refuses to pick up
             # g.config['ticket_provider'] value, so hardcoding for now.
             # really, the next uncommented line should be:
             #self.ticket_provider,
@@ -1074,7 +1074,7 @@ class Globals(object):
 
     def __del__(self):
         """
-        Put any cleanup code to be run when the application finally exits 
+        Put any cleanup code to be run when the application finally exits
         here.
         """
         pass
